122397d0659f625ddf55d7b59341a4f4ffb0eeb7
Vasundhara <va308@cam.ac.uk>
2019-05-16 14:31:53 +0100

    SavedLocations

---
+++ uk/ac/cl/interactionDesign/group1/SavedLocations.java
@@ -0,0 +1,20 @@
+package uk.ac.cl.interactionDesign.group1;
+import sun.applet.Main;
+
+import javax.swing.*;
+import java.awt.*;
+import javax.imageio.*;
+import java.io.File;
+import java.io.IOException;
+
+public class SavedLocations {
+
+
+
+    public static void main(String args[])throws IOException
+    {
+
+
+        new MainFrame();
+    }
+}
