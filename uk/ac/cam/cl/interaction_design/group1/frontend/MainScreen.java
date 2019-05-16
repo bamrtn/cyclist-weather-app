a61ec5ac34572b0243ed9dec43d71d4897ae9435
Vasundhara <va308@cam.ac.uk>
2019-05-16 14:38:07 +0100

    changed package names

---
+++ uk/ac/cam/cl/interaction_design/group1/frontend/MainScreen.java
@@ -0,0 +1,6 @@
+package uk.ac.cam. cl.interaction_design.group1.frontend;
+
+import javax.swing.*;
+
+public class MainScreen extends JLabel {
+}
---
+++ uk/ac/cam/cl/interaction_design/group1/frontend/SavedLocations.java
@@ -0,0 +1,20 @@
+package uk.ac.cam. cl.interaction_design.group1.frontend;
+import sun.applet.Main;
+
+import javax.swing.*;
+import java.awt.*;
+import javax.imageio.*;
+import java.io.File;
+import java.io.IOException;
+
+public class SavedLocations {
+
+
+
+    public static void main(String args[])throws IOException
+    {
+
+
+        new MainFrame();
+    }
+}
